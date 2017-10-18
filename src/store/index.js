92bbc5593c85129692a073d4d1f7d91dc3af5b61
Andrey Savchenko <korsap@bk.ru>
2017-10-18 17:44:07 +0500

    Home Task 5 is done

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import reducer from '../reducer';
 import logger from '../middlewares/logger';
 import idGenerator from '../middlewares/idGenerator';
 
-const enchancer = applyMiddleware(logger, idGenerator);
+const enchancer = applyMiddleware(idGenerator, logger );
 
 const store = createStore(reducer, enchancer);
 
