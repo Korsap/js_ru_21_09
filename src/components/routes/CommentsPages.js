84e7ca6756d82f841cfed7f7f54b7fda0fa9bd3b
Andrey Savchenko <korsap@bk.ru>
2017-10-25 01:15:51 +0500

    HT7

---
+++ src/components/routes/CommentsPages.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {Route} from 'react-router-dom'
+import CommentList from '../CommentList'
+import Comment from '../Comment'
+
+class CommentsPages extends Component {
+
+}
+
+export default CommentsPages
